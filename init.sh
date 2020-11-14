ee642f0362421eb4fc46d29120cec4129a2200d3
Logan Ballard <loganballard@gmail.com>
2020-11-14 10:02:32 -0800

    updating script

--- init.sh
+++ init.sh
@@ -5,13 +5,11 @@
 GCP_ZONES="us-west1-a,us-west1-b,us-west1-c"
 PROJECT=`gcloud config get-value project`
 CLUSTER_NAME=simple.k8s.local
+NODE_SIZE="n1-standard-8"
+NODE_COUNT=6
+MASTER_SIZE="n1-standard-4"
 
 # Optional Flags
-NODE_COUNT_FLAG="--node-count 4"
-MASTER_COUNT_FLAG="--master-count 3"
-NODE_SIZE_FLAG="--node-size n2-standard-8"
-MASTER_SIZE_FLAG="--master-size n2-standard-4"
-GCP_MASTER_ZONES_FLAG="--master-zones us-west1-a,us-west1-b,us-west1-c"
 if [[ ! -z $1 ]] ; then DRY_RUN_FLAG='--dry-run -o yaml' ; fi
 
 cd terraform && \
@@ -24,25 +22,16 @@ kops create cluster \
     --name=${CLUSTER_NAME} \
     --zones ${GCP_ZONES} \
     --state ${KOPS_STATE_STORE} \
+    --node-count ${NODE_COUNT} \
     --project=${PROJECT} \
-    ${NODE_COUNT_FLAG} \
-    ${MASTER_COUNT_FLAG} \
-    ${NODE_SIZE_FLAG} \
-    ${MASTER_SIZE_FLAG} \
-    ${GCP_MASTER_ZONES_FLAG} \
     ${DRY_RUN_FLAG}
 
 if [[ -z ${DRY_RUN_FLAG} ]] ; then
 
-    clear && echo "cluster info: " && sleep 2
-    # Ensure was created
-    kops get cluster --state ${KOPS_STATE_STORE} && sleep 2
+    kops get cluster --name ${CLUSTER_NAME} --state ${KOPS_STATE_STORE} -oyaml && sleep 5
 
-    clear && echo "cluster details: " && sleep 2
-    kops get cluster --state ${KOPS_STATE_STORE}/ ${CLUSTER_NAME} -oyaml && sleep 2
+    kops update cluster --name ${CLUSTER_NAME} --state ${KOPS_STATE_STORE} --yes && sleep 5
 
-    clear && echo "instancegroup info:" && sleep 2
-    kops get instancegroup --state ${KOPS_STATE_STORE}/ --name ${CLUSTER_NAME} && sleep 2
-else
-    echo "dry run"
+    kops validate cluster --name ${CLUSTER_NAME} --state ${KOPS_STATE_STORE} --wait 10m
+
 fi
