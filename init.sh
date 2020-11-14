5501b314dd7634d56b29e52a7da22f15e9f08d22
Logan Ballard <loganballard@gmail.com>
2020-11-14 07:46:39 -0800

    updating init script

--- init.sh
+++ init.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+# set -x
 
-GCP_ZONES=us-west1-c
+# basic info
+GCP_ZONES="us-west1-a,us-west1-b,us-west1-c"
 PROJECT=`gcloud config get-value project`
 CLUSTER_NAME=simple.k8s.local
+
+# Optional Flags
+NODE_COUNT_FLAG="--node-count 4"
+MASTER_COUNT_FLAG="--master-count 3"
+NODE_SIZE_FLAG="--node-size n2-standard-8"
+MASTER_SIZE_FLAG="--master-size n2-standard-4"
+GCP_MASTER_ZONES_FLAG="--master-zones us-west1-a,us-west1-b,us-west1-c"
+if [[ ! -z $1 ]] ; then DRY_RUN_FLAG='--dry-run -o yaml' ; fi
 
 cd terraform && \
     KOPS_STATE_STORE="gs://"`terraform output -json | jq .kops_bucket.value | tail -c +2 | head -c -2`"/" \
@@ -10,15 +20,29 @@ cd terraform && \
 
 # to unlock the GCE features 
 export KOPS_FEATURE_FLAGS=AlphaAllowGCE 
-kops create cluster ${CLUSTER_NAME} --zones ${GCP_ZONES} --state ${KOPS_STATE_STORE} --project=${PROJECT}
+kops create cluster \
+    --name=${CLUSTER_NAME} \
+    --zones ${GCP_ZONES} \
+    --state ${KOPS_STATE_STORE} \
+    --project=${PROJECT} \
+    ${NODE_COUNT_FLAG} \
+    ${MASTER_COUNT_FLAG} \
+    ${NODE_SIZE_FLAG} \
+    ${MASTER_SIZE_FLAG} \
+    ${GCP_MASTER_ZONES_FLAG} \
+    ${DRY_RUN_FLAG}
 
-clear && echo "cluster info: " && sleep 2
-# Ensure was created
-kops get cluster --state ${KOPS_STATE_STORE} && sleep 2
+if [[ -z ${DRY_RUN_FLAG} ]] ; then
 
-clear && echo "cluster details: " && sleep 2
-kops get cluster --state ${KOPS_STATE_STORE}/ ${CLUSTER_NAME} -oyaml && sleep 2
+    clear && echo "cluster info: " && sleep 2
+    # Ensure was created
+    kops get cluster --state ${KOPS_STATE_STORE} && sleep 2
 
-clear && echo "instancegroup info:" && sleep 2
-kops get instancegroup --state ${KOPS_STATE_STORE}/ --name ${CLUSTER_NAME} && sleep 2
+    clear && echo "cluster details: " && sleep 2
+    kops get cluster --state ${KOPS_STATE_STORE}/ ${CLUSTER_NAME} -oyaml && sleep 2
 
+    clear && echo "instancegroup info:" && sleep 2
+    kops get instancegroup --state ${KOPS_STATE_STORE}/ --name ${CLUSTER_NAME} && sleep 2
+else
+    echo "dry run"
+fi
